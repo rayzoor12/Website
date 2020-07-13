9efb5dcff7f9203352565d87f4b7068e18b590fd
Emil Fjellström <emilwow22@gmail.com>
2020-07-13 20:51:45 +0200

    remove ssrReveal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,6 @@ import SEO from "../components/seo"
 import Hero from "../components/Hero/Hero"
 import Intro from "../components/Intro/Intro"
 import ProjectContainer from "../components/ProjectContainer/ProjectContainer"
-import config from "react-reveal/globals"
-
-config({ ssrReveal: true })
 
 const IndexPage = () => (
   <Layout>
